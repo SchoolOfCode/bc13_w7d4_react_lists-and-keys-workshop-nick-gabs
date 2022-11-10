1e4a269b6a3c49e9e81c94c82806d6feb5000222
Gabriel Brocklesby <gabrielbrocklesby@Gabriels-MacBook-Pro.local>
2022-11-10 16:25:22 +0000

    task 4 complete

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -10,8 +10,8 @@ function Main() {
   const [posts, setPosts] = useState(samplePosts);
  
 
-  return <main id="main"> {posts.map((post)=>{
-    return <post/>
+  return <main id="main"> {posts.map(({postId,title, date, author, test, highlights})=>{
+    return <Post key={postId} title={title} date={date} author={author} test={test} highlights={highlights}/>
   })}</main>;
 }
 
